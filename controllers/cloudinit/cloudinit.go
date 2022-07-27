e195599e8f102c6f120b868b4dfe631bd1f248dc
Ferran Llamas <llamas.arroniz@gmail.com>
2022-07-27 15:47:31 +0200

    Cleanup cloudinit (#11)
    Co-authored-by: Konstantinos Tsakalozos <konstantinos.tsakalozos@canonical.com>

--- controllers/cloudinit/cloudinit.go
+++ controllers/cloudinit/cloudinit.go
@@ -43,14 +43,8 @@ const (
 // BaseUserData is shared across all the various types of files written to disk.
 type BaseUserData struct {
 	Header               string
-	PreKubeadmCommands   []string
-	PostKubeadmCommands  []string
 	AdditionalFiles      []bootstrapv1.File
 	WriteFiles           []bootstrapv1.File
-	Users                []bootstrapv1.User
-	NTP                  *bootstrapv1.NTP
-	DiskSetup            *bootstrapv1.DiskSetup
-	Mounts               []bootstrapv1.MountPoints
 	ControlPlane         bool
 	UseExperimentalRetry bool
 	KubeadmCommand       string
@@ -78,30 +72,6 @@ func generate(kind string, tpl string, data interface{}) ([]byte, error) {
 	tm := template.New(kind).Funcs(defaultTemplateFuncMap)
 	if _, err := tm.Parse(filesTemplate); err != nil {
 		return nil, errors.Wrap(err, "failed to parse files template")
-	}
-
-	if _, err := tm.Parse(commandsTemplate); err != nil {
-		return nil, errors.Wrap(err, "failed to parse commands template")
-	}
-
-	if _, err := tm.Parse(ntpTemplate); err != nil {
-		return nil, errors.Wrap(err, "failed to parse ntp template")
-	}
-
-	if _, err := tm.Parse(usersTemplate); err != nil {
-		return nil, errors.Wrap(err, "failed to parse users template")
-	}
-
-	if _, err := tm.Parse(diskSetupTemplate); err != nil {
-		return nil, errors.Wrap(err, "failed to parse disk setup template")
-	}
-
-	if _, err := tm.Parse(fsSetupTemplate); err != nil {
-		return nil, errors.Wrap(err, "failed to parse fs setup template")
-	}
-
-	if _, err := tm.Parse(mountsTemplate); err != nil {
-		return nil, errors.Wrap(err, "failed to parse mounts template")
 	}
 
 	t, err := tm.Parse(tpl)
