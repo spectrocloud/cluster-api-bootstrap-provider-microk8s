d1b56350b7832b85e3bf33941bed86164dd4fb65
Konstantinos Tsakalozos <kos.tsakalozos@canonical.com>
2022-08-02 16:37:11 +0300

    clusterctl integration (#14)
    * clusterctl integration
    
    * Working metadata and bootstrap-components yamls
    
    * Build the components file from the makefile
    
    * Readme updates with diagrams

--- main.go
+++ main.go
@@ -19,21 +19,16 @@ package main
 import (
 	"context"
 	"flag"
-	"fmt"
 	"math/rand"
 	"os"
 	"time"
 
 	corev1 "k8s.io/api/core/v1"
-	cliflag "k8s.io/component-base/cli/flag"
 
 	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
 	// to ensure that exec-entrypoint and run can make use of them.
 	"github.com/canonical/cluster-api-bootstrap-provider-microk8s/controllers"
-	"github.com/spf13/pflag"
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
-	"k8s.io/client-go/tools/leaderelection/resourcelock"
-	"k8s.io/component-base/logs"
 	"k8s.io/klog/v2/klogr"
 
 	"k8s.io/apimachinery/pkg/runtime"
@@ -41,10 +36,8 @@ import (
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	clusterv1 "sigs.k8s.io/cluster-api/api/v1beta1"
 	"sigs.k8s.io/cluster-api/controllers/remote"
-	"sigs.k8s.io/cluster-api/feature"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
@@ -72,85 +65,7 @@ func init() {
 	rand.Seed(time.Now().UnixNano())
 }
 
-var (
-	metricsBindAddr             string
-	enableLeaderElection        bool
-	leaderElectionLeaseDuration time.Duration
-	leaderElectionRenewDeadline time.Duration
-	leaderElectionRetryPeriod   time.Duration
-	watchFilterValue            string
-	watchNamespace              string
-	profilerAddress             string
-	controlPlaneConcurrency     int
-	syncPeriod                  time.Duration
-	webhookPort                 int
-	webhookCertDir              string
-	healthAddr                  string
-	tokenTTL                    time.Duration
-	logOptions                  = logs.NewOptions()
-)
-
-// InitFlags initializes this manager's flags.
-func InitFlags(fs *pflag.FlagSet) {
-	logs.AddFlags(fs, logs.SkipLoggingConfigurationFlags())
-	logOptions.AddFlags(fs)
-
-	fs.StringVar(&metricsBindAddr, "metrics-bind-addr", "localhost:8080",
-		"The address the metric endpoint binds to.")
-
-	fs.BoolVar(&enableLeaderElection, "leader-elect", false,
-		"Enable leader election for controller manager. Enabling this will ensure there is only one active controller manager.")
-
-	fs.DurationVar(&leaderElectionLeaseDuration, "leader-elect-lease-duration", 15*time.Second,
-		"Interval at which non-leader candidates will wait to force acquire leadership (duration string)")
-
-	fs.DurationVar(&leaderElectionRenewDeadline, "leader-elect-renew-deadline", 10*time.Second,
-		"Duration that the leading controller manager will retry refreshing leadership before giving up (duration string)")
-
-	fs.DurationVar(&leaderElectionRetryPeriod, "leader-elect-retry-period", 2*time.Second,
-		"Duration the LeaderElector clients should wait between tries of actions (duration string)")
-
-	fs.StringVar(&watchNamespace, "namespace", "",
-		"Namespace that the controller watches to reconcile cluster-api objects. If unspecified, the controller watches for cluster-api objects across all namespaces.")
-
-	fs.StringVar(&profilerAddress, "profiler-address", "",
-		"Bind address to expose the pprof profiler (e.g. localhost:6060)")
-
-	fs.IntVar(&controlPlaneConcurrency, "concurrency", 10,
-		"Number of kubeadm configs to process simultaneously")
-
-	fs.DurationVar(&syncPeriod, "sync-period", 10*time.Minute,
-		"The minimum interval at which watched resources are reconciled (e.g. 15m)")
-
-	fs.StringVar(&watchFilterValue, "watch-filter", "",
-		fmt.Sprintf("Label value that the controller watches to reconcile cluster-api objects. Label key is always %s. If unspecified, the controller watches for all cluster-api objects.", clusterv1.WatchLabel))
-
-	fs.IntVar(&webhookPort, "webhook-port", 9443,
-		"Webhook Server port")
-
-	fs.StringVar(&webhookCertDir, "webhook-cert-dir", "/tmp/k8s-webhook-server/serving-certs/",
-		"Webhook cert dir, only used when webhook-port is specified.")
-
-	fs.StringVar(&healthAddr, "health-addr", ":9440",
-		"The address the health endpoint binds to.")
-
-	feature.MutableGates.AddFlag(fs)
-}
-
 func main() {
-
-	rand.Seed(time.Now().UnixNano())
-
-	InitFlags(pflag.CommandLine)
-	pflag.CommandLine.SetNormalizeFunc(cliflag.WordSepNormalizeFunc)
-	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
-	pflag.Parse()
-
-	if err := logOptions.ValidateAndApply(); err != nil {
-		setupLog.Error(err, "unable to start manager")
-		os.Exit(1)
-	}
-
 	// Set the Klog format, as the Serialize format shouldn't be used anymore.
 	// This makes sure that the logs are formatted correctly, i.e.:
 	// * JSON logging format: msg isn't serialized twice
@@ -175,16 +90,11 @@ func main() {
 	restConfig := ctrl.GetConfigOrDie()
 	restConfig.UserAgent = remote.DefaultClusterAPIUserAgent("cluster-api-microk8s-bootstrap-manager")
 	mgr, err := ctrl.NewManager(restConfig, ctrl.Options{
-		Scheme:                     scheme,
-		MetricsBindAddress:         metricsBindAddr,
-		LeaderElection:             enableLeaderElection,
-		LeaderElectionID:           "microk8s-bootstrap-manager-leader-election-capi",
-		LeaseDuration:              &leaderElectionLeaseDuration,
-		RenewDeadline:              &leaderElectionRenewDeadline,
-		RetryPeriod:                &leaderElectionRetryPeriod,
-		LeaderElectionResourceLock: resourcelock.LeasesResourceLock,
-		Namespace:                  watchNamespace,
-		SyncPeriod:                 &syncPeriod,
+		Scheme:                 scheme,
+		MetricsBindAddress:     metricsAddr,
+		HealthProbeBindAddress: probeAddr,
+		LeaderElection:         enableLeaderElection,
+		LeaderElectionID:       "microk8s-bootstrap-manager-leader-election-capi",
 		ClientDisableCacheFor: []client.Object{
 			&corev1.ConfigMap{},
 			&corev1.Secret{},
@@ -219,8 +129,4 @@ func main() {
 		setupLog.Error(err, "problem running manager")
 		os.Exit(1)
 	}
-}
-
-func concurrency(c int) controller.Options {
-	return controller.Options{MaxConcurrentReconciles: c}
 }
